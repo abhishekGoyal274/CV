dd162c31b71d929b55a4029e3e74c546c495d303
abhishekGoyal274 <51013750+abhishekGoyal274@users.noreply.github.com>
2021-08-06 22:15:52 +0530

    Try 3rd Time

--- javascript.js
+++ javascript.js
@@ -3,14 +3,8 @@ var player1, player2;
 player1 = Math.ceil(Math.random() * 5 + 1);
 player2 = Math.ceil(Math.random() * 5 + 1);
 
-// Random backgroundColor variables
-var setBackgroundColor0 = Math.floor(Math.random() * 256);
-var setBackgroundColor1 = Math.floor(Math.random() * 256);
-var setBackgroundColor2 = Math.floor(Math.random() * 256);
-var setBackgroundColor3 = Math.random();
 
 // Player winner
-document.body.style.backgroundColor = "rgba(" + setBackgroundColor0 + "," + setBackgroundColor1 + "," + setBackgroundColor2 + "," + setBackgroundColor3 + ")";
 if (player1 > player2) {
     document.getElementsByTagName("h1")[0].textContent = "🙋‍♂️Player 1🤦‍♂️";
 } else if (player2 > player1) {
@@ -22,3 +16,11 @@ if (player1 > player2) {
 //Dice image Changer
 document.getElementsByClassName("img1")[0].setAttribute("src", "dice" + player1 + ".png")
 document.getElementsByClassName("img2")[0].setAttribute("src", "dice" + player2 + ".png")
+
+
+// Random backgroundColor variables
+var setBackgroundColor0 = Math.floor(Math.random() * 256);
+var setBackgroundColor1 = Math.floor(Math.random() * 256);
+var setBackgroundColor2 = Math.floor(Math.random() * 256);
+var setBackgroundColor3 = Math.random();
+document.body.style.backgroundColor = "rgba(" + setBackgroundColor0 + "," + setBackgroundColor1 + "," + setBackgroundColor2 + "," + setBackgroundColor3 + ")";
