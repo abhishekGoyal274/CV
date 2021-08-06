ea7c8a99082d48370e84cc09bee116132edd3a53
abhishekGoyal274 <51013750+abhishekGoyal274@users.noreply.github.com>
2021-08-06 22:11:20 +0530

    changes images path

---
+++ javascript.js
@@ -0,0 +1,24 @@
+//Random Dice number generator
+var player1, player2;
+player1 = Math.ceil(Math.random() * 5 + 1);
+player2 = Math.ceil(Math.random() * 5 + 1);
+
+// Random backgroundColor variables
+var setBackgroundColor0 = Math.floor(Math.random() * 256);
+var setBackgroundColor1 = Math.floor(Math.random() * 256);
+var setBackgroundColor2 = Math.floor(Math.random() * 256);
+var setBackgroundColor3 = Math.random();
+
+// Player winner
+document.body.style.backgroundColor = "rgba(" + setBackgroundColor0 + "," + setBackgroundColor1 + "," + setBackgroundColor2 + "," + setBackgroundColor3 + ")";
+if (player1 > player2) {
+    document.getElementsByTagName("h1")[0].textContent = "🙋‍♂️Player 1🤦‍♂️";
+} else if (player2 > player1) {
+    document.getElementsByTagName("h1")[0].textContent = "🤦‍♂️Player 2🙋‍♂️";
+} else {
+    document.getElementsByTagName("h1")[0].textContent = "🎯 Draw 🎯";
+}
+
+//Dice image Changer
+document.getElementsByClassName("img1")[0].setAttribute("src", "dice" + player1 + ".png")
+document.getElementsByClassName("img2")[0].setAttribute("src", "dice" + player2 + ".png")
